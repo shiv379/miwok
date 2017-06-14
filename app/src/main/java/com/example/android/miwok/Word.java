634b271f5986af60486dc48167d747cb02b43c68
Toby King <shiv379@gmail.com>
2017-06-14 21:37:13 +0100

    Added audio

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 /**
@@ -11,16 +12,24 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mSoundFileResourceId;
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int soundFileResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mSoundFileResourceId = soundFileResourceId;
     }
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+    public Word(String defaultTranslation, String miwokTranslation, int soundFileResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mSoundFileResourceId = soundFileResourceId;
     }
 
     public String getDefaultTranslation(){
@@ -33,5 +42,8 @@ public class Word {
 
     public int getImageResourceId(){return mImageResourceId; }
 
+    public int getSoundFileResourceId(){ return mSoundFileResourceId; }
+
     public boolean hasImage(){ return mImageResourceId != NO_IMAGE_PROVIDED; }
+
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -9,12 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -44,9 +40,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the {@link Word} object located at this position in the list
         Word word = getItem(position);
 
+        TextView playIcon = (TextView) listItemView.findViewById(R.id.play_icon);
         LinearLayout textLinearLayout = (LinearLayout) listItemView.findViewById(R.id.textLinearLayout);
         int color = ContextCompat.getColor(getContext(), mActivityColor);
         textLinearLayout.setBackgroundColor(color);
+        playIcon.setBackgroundColor(color);
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(word.getDefaultTranslation());
@@ -62,7 +60,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
             wordImageView.setVisibility(View.GONE);
         }
 
-        // Return the whole list item layout (containing 2 TextViews)
+        // Return the whole list item layout
         // so that it can be shown in the ListView
         return listItemView;
     }
