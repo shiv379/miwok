1574a4e5cab4a0c62098d1709c27390f106e3aff
Toby King <shiv379@gmail.com>
2017-07-01 14:13:43 +0100

    Converted NumbersActivity to Fragment

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,115 +1,16 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class NumbersActivity extends AppCompatActivity {
-
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                // Pause playback and reset player to the start of the file.
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                //Stop playback and clean up
-                releaseMediaPlayer();
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                //Resume playback
-                mMediaPlayer.start();
-            }
-        }
-    };
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener(){
-        @Override
-        public void onCompletion(MediaPlayer mp){
-            releaseMediaPlayer();
-        }
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> words = new ArrayList<Word>();
-
-        words.add(new Word("one","lutti",R.drawable.number_one,R.raw.number_one));
-        words.add(new Word("two","otiiko",R.drawable.number_two,R.raw.number_two));
-        words.add(new Word("three","tolookosu",R.drawable.number_three,R.raw.number_three));
-        words.add(new Word("four","oyyisa",R.drawable.number_four,R.raw.number_four));
-        words.add(new Word("five","massokka",R.drawable.number_five,R.raw.number_five));
-        words.add(new Word("six","temmokka",R.drawable.number_six,R.raw.number_six));
-        words.add(new Word("seven","kenekaku",R.drawable.number_seven,R.raw.number_seven));
-        words.add(new Word("eight","kawinta",R.drawable.number_eight,R.raw.number_eight));
-        words.add(new Word("nine","wo'e",R.drawable.number_nine,R.raw.number_nine));
-        words.add(new Word("ten","na'aacha",R.drawable.number_ten,R.raw.number_ten));
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_numbers);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(itemsAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                 releaseMediaPlayer();
-
-                 int result = mAudioManager.requestAudioFocus(
-                         mOnAudioFocusChangeListener,
-                         AudioManager.STREAM_MUSIC,
-                         AudioManager.AUDIOFOCUS_GAIN);
-                 if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                     //play music
-                     mMediaPlayer = MediaPlayer.create(NumbersActivity.this, words.get(i).getSoundFileResourceId());
-                     mMediaPlayer.start();
-                     mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                 }
-             }
-        });
-    }
-
-    private void releaseMediaPlayer(){
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        releaseMediaPlayer();
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new NumbersFragment())
+                .commit();
     }
 }
